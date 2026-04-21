1cd3aa030f1f112079d36a4fcd6ec5e5426f6a13
imtinan <53833314+imtinan39@users.noreply.github.com>
2026-04-21 11:32:26 -0400

    Add: sort-colors - submission-14

---
+++ Data Structures & Algorithms/sort-colors/submission-14.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        count=[0]*3
+        print(count)
+        for i in nums:
+            count[i]+=1
+        k=0
+        
+        for i in range(len(count)):
+            for j in range(count[i]):
+                nums[k]=i
+                k+=1
+        
+        
