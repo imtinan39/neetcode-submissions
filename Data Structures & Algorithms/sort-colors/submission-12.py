e4780798bc5f27980666d98884fba7c011534c07
imtinan <53833314+imtinan39@users.noreply.github.com>
2026-04-21 10:11:22 -0400

    Add: sort-colors - submission-12

---
+++ Data Structures & Algorithms/sort-colors/submission-12.py
@@ -0,0 +1,24 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zero=0
+        one=0
+        two=0
+        for i in nums:
+            if i==0:
+                zero+=1
+            elif i==1:
+                one+=1
+            elif i==2:
+                two+=1
+
+        for i in range(0,zero):
+            nums[i]=0
+        for j in range(zero, zero+one):
+            nums[j]=1
+        for k in range(zero+one,zero+one+two):
+            nums[k]=2 # spece complexity is one as it does not create tempory list
+        
+        
