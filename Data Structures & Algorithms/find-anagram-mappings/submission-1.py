d3783a11b2998458f7fc2874456a05449eda7c1e
imtinan <53833314+imtinan39@users.noreply.github.com>
2026-05-02 13:19:56 -0400

    Add: find-anagram-mappings - submission-1

---
+++ Data Structures & Algorithms/find-anagram-mappings/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def anagramMappings(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        hashmap={}
+        for i,j in enumerate(nums2):
+            hashmap[j]=i
+
+        return [hashmap[k] for k in nums1]
+
+        
+        
+        
