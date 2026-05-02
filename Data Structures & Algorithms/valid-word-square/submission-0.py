a9d8cfb169fa0c05d69966ac76bbd3b68abaa99d
imtinan <53833314+imtinan39@users.noreply.github.com>
2026-05-01 20:45:41 -0400

    Add: valid-word-square - submission-0

---
+++ Data Structures & Algorithms/valid-word-square/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def validWordSquare(self, words: List[str]) -> bool:
+        hashmap={}
+        for w in range(len(words)):
+            for i in range(len(words[w])):
+                hashmap[i]=hashmap.get(i,"")+words[w][i]
+        
+        
+        for w in range(len(words)):
+            if words[w]==hashmap[w]:
+                continue
+            else:
+                return False
+        return True
+        
