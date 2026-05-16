4a311372aea1d9a8b2a1982a17f43c01042590c4
imtinan <53833314+imtinan39@users.noreply.github.com>
2026-05-16 00:57:47 -0400

    Add: check-if-a-number-is-majority-element-in-a-sorted-array - submission-5

---
+++ Data Structures & Algorithms/check-if-a-number-is-majority-element-in-a-sorted-array/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isMajorityElement(self, nums: List[int], target: int) -> bool:
+
+        l=0
+        r=len(nums)-1
+        index=0
+        while l<=r:
+            mid=(l+r)//2
+
+            if nums[mid]==target:
+                index=mid
+                r=mid-1
+            elif nums[mid]>target:
+                r=mid-1
+            elif nums[mid]<target:
+                l=mid+1
+        return index+(len(nums)//2)<len(nums) and nums[index+len(nums)//2]==target
+
+
+
+        
