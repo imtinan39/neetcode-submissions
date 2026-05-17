cdceb7bd8e0ed302747303051a823f694ec0ef2f
imtinan <53833314+imtinan39@users.noreply.github.com>
2026-05-16 20:32:22 -0400

    Add: missing-element-in-sorted-array - submission-19

---
+++ Data Structures & Algorithms/missing-element-in-sorted-array/submission-19.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def missingElement(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        left, right = 0, n
+
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] - (nums[0] + mid) < k:
+                left = mid + 1
+            else:
+                right = mid
+
+        return nums[0] + k + left - 1
