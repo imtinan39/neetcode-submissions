585ea5418f66d67b2316511d48db896c45b338e4
imtinan <53833314+imtinan39@users.noreply.github.com>
2026-05-16 23:16:01 -0400

    Add: lonely-pixel-i - submission-6

---
+++ Data Structures & Algorithms/lonely-pixel-i/submission-6.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findLonelyPixel(self, picture: List[List[str]]) -> int:
+        hash_col={}
+        hash_rows={}
+        count=0
+
+        for i in range(len(picture)):
+            for j in range(len(picture[i])):
+                if picture[i][j]=="B":
+                    hash_col[j]=hash_col.get(j,0)+1
+                    hash_rows[i]=hash_rows.get(i,0)+1
+                else:
+                    hash_col[j]=hash_col.get(j,0)
+                    hash_rows[i]=hash_rows.get(i,0)
+
+        for i in range(len(picture)):
+            if hash_rows[i]!=1:
+                continue
+            for j in range(len(picture[i])):
+                if picture[i][j]=="B" and hash_col[j]==1:
+                    count+=1
+        return count
+
+        
