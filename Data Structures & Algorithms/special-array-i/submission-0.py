5941baec57999340f1713852aa920f324222b78b
imtinan <53833314+imtinan39@users.noreply.github.com>
2026-05-17 20:28:00 -0400

    Add: special-array-i - submission-0

---
+++ Data Structures & Algorithms/special-array-i/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+
+        if len(nums)==1:
+            return True
+        
+        for i in range(len(nums)-1):
+            if (nums[i]+nums[i+1])%2!=0:
+                continue
+            else:
+                return False
+
+        return True
