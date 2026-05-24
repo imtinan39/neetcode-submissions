1795eab38b70e5cbf921bd8a01b108a92538c7b1
imtinan <53833314+imtinan39@users.noreply.github.com>
2026-05-24 18:09:03 -0400

    Add: permutation-string - submission-15

---
+++ Data Structures & Algorithms/permutation-string/submission-15.py
@@ -0,0 +1,28 @@
+import copy
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        dq=deque(maxlen=len(s1))
+        l=0
+        count=0
+        hashmap={}
+        for i in s1:
+            hashmap[i]=hashmap.get(i,0)+1
+
+        for  r in range(len(s2)):
+            
+            dq.append(s2[r])
+            if len(dq)==len(s1):
+                d2 = copy.deepcopy(hashmap)
+                for d in dq:
+                    if d in d2 and d2[d]!=0:
+                        count+=1
+                        d2[d]-=1
+                    else:
+                        count=0
+                        break
+                
+                if count==len(s1):
+                    return True
+        return False
+        
