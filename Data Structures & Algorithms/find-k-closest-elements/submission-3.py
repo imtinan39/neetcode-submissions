e14bbb8ff583d9d302d627b3af5f56a6609f7b5a
imtinan <53833314+imtinan39@users.noreply.github.com>
2026-05-24 21:38:13 -0400

    Add: find-k-closest-elements - submission-3

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-3.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+
+        hashmap={}
+
+        for i in arr:
+            hashmap[i]=(abs(i-x),i)
+        
+        sorted_items = sorted(hashmap.items(), key=lambda item: item[1])
+        print(sorted_items)
+        count=0
+        res=[]
+        count=0
+        hashmap_2={}
+
+        for i in arr:
+            hashmap_2[i]=hashmap_2.get(i,0)+1
+        
+        for i in sorted_items:
+            for j in range(hashmap_2[i[0]]):
+                res.append(i[0])
+                count+=1
+                if count==k:
+                    break
+            if count==k:
+                    break
+        return sorted(res)
+
+
+
+        
