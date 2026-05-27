5225697c43935ce4770a59399c2e9a28ef98cc88
imtinan <53833314+imtinan39@users.noreply.github.com>
2026-05-27 15:22:07 -0400

    Add: find-k-length-substrings-with-no-repeated-characters - submission-1

---
+++ Data Structures & Algorithms/find-k-length-substrings-with-no-repeated-characters/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numKLenSubstrNoRepeats(self, s: str, k: int) -> int:
+
+        hashmap={}
+        l=0
+        ans=0
+
+        for r in range(len(s)):
+
+            if s[r] in hashmap:
+                while s[r] in hashmap:
+                    hashmap[s[l]]-=1
+                    if hashmap[s[l]]==0:
+                        del hashmap[s[l]]
+                    l+=1
+            hashmap[s[r]]=hashmap.get(s[r],0)+1
+
+            if r-l+1==k:
+                if len(hashmap)==k:
+                    ans+=1
+                    hashmap[s[l]]-=1
+                    if hashmap[s[l]]==0:
+                        del hashmap[s[l]]
+                    l+=1
+        return ans
+
+
+        
