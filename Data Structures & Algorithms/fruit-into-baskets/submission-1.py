d02843318b544c26d6ae216b523d10ac7e00894d
imtinan <53833314+imtinan39@users.noreply.github.com>
2026-05-28 10:47:49 -0400

    Add: fruit-into-baskets - submission-1

---
+++ Data Structures & Algorithms/fruit-into-baskets/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        l=0
+        hashmap={}
+        ans=0
+
+        for r in range(len(fruits)):
+            hashmap[fruits[r]]=hashmap.get(fruits[r],0)+1
+
+            while len(hashmap)>2:
+                hashmap[fruits[l]]-=1
+
+                if hashmap[fruits[l]]==0:
+                    del hashmap[fruits[l]]
+                l+=1
+            
+            ans=max(ans,r-l+1)
+        
+        return ans
