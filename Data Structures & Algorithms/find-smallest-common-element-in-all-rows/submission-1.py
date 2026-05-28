06472048cfd554fa947f5b2213612ab3eb0584b5
imtinan <53833314+imtinan39@users.noreply.github.com>
2026-05-28 15:57:11 -0400

    Add: find-smallest-common-element-in-all-rows - submission-1

---
+++ Data Structures & Algorithms/find-smallest-common-element-in-all-rows/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def smallestCommonElement(self, mat: List[List[int]]) -> int:
+
+        common = list(set.intersection(*(set(lst) for lst in mat)))
+        
+        return min(common) if len(common)!=0 else -1
+        
+      
+
+        
