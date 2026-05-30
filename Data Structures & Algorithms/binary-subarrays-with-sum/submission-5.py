3834e03a1817e8585329744f342ff7be26badc9d
imtinan <53833314+imtinan39@users.noreply.github.com>
2026-05-29 20:54:13 -0400

    Add: binary-subarrays-with-sum - submission-5

---
+++ Data Structures & Algorithms/binary-subarrays-with-sum/submission-5.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        def helper(x):
+            if x<0:
+                return 0
+            l=0
+            cur=0
+            result=0
+
+            for r in range(len(nums)):
+                cur+=nums[r]
+
+                while cur>x:
+                    cur-=nums[l]
+                    l+=1
+                result+=(r-l+1)
+            return result
+        return helper(goal)-helper(goal-1)
+        
+
+        
+
+        
