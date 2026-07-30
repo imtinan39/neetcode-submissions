35f3c95bd2f879c1dc01d893d90e221b4e31bf15
imtinan <53833314+imtinan39@users.noreply.github.com>
2026-07-30 16:56:31 -0400

    Add: max-consecutive-ones-ii - submission-22

---
+++ Data Structures & Algorithms/max-consecutive-ones-ii/submission-22.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        length=0
+        l=0
+        dic={}
+        count=0
+        for r,n in enumerate(nums):
+            dic[n]=dic.get(n,0)+1
+            if n==0:
+                count+=1
+            while count and dic[0]>1:
+                dic[nums[l]]-=1
+                if dic[nums[l]]==0:
+                    del dic[nums[l]]
+                    count=0
+                l+=1
+            length=max(length,r-l+1)
+        return length
+
+
+
+
+
+        
